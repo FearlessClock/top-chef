24df0c72dd0778120ab3bac78bc175e824aa1334
Pieter ALGERA <pieter.algera@devinci.fr>
2018-02-13 08:37:33 +0100

    Feat: Create lafourchette deal getter

---
+++ lafourchette.js
@@ -0,0 +1,24 @@
+var fs      = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+
+function loadDeals(names){
+    let url = "https://www.lafourchette.com/search-refine/"
+    names.forEach(element => {
+        let requestedUrl = url + element;
+        request(requestedUrl, function(error, response, html){
+            if(!error){
+                let $ = cheerio.load(html)
+
+                console.log($('li.resultItem:nth-child(1)').attr('data-restaurant-id'))
+            }
+        })
+    });
+}
+
+function getDeals(){
+
+
+}
+
+module.exports = {loadDeals, getDeals}
