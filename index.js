040ff622ffbe37ed63fd6a0a2acfe310c83a7a7d
Pieter ALGERA <pieter.algera@devinci.fr>
2018-02-16 17:37:05 +0100

    Feat: Used promises to wait till the end of the requests

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs      = require('fs');
 var request = require('request');
+var rp = require('request-promise');
 var cheerio = require('cheerio');
 
 var mongoose = require('mongoose');
@@ -22,61 +23,53 @@ function getStarredRestoNames(){
   let restoName = mongoose.model('RestoName', restoNameSchema);
 
   url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin';
-
+  var options = {
+    uri: url,
+    transform: function (body) {
+        return cheerio.load(body);
+    }
+  };
   // The structure of our request call
   // The first parameter is our URL
   // The callback function takes 3 parameters, an error, response status code and the html
   var nmbrOfPages = 0
-  request(url, function(error, response, html){
+  rp(options).then( function($){
     // First we'll check to make sure no errors occurred when making the request
-
-    if(!error){
       // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
 
-      var $ = cheerio.load(html);
-      $('div.pager-wrapper div.item-list-first div.item-list .pager').filter(function(){
-        nmbrOfPages = $(this).children().last().prev().children().attr('attr-page-number')
-      })
-      $('.poi-search-result').filter(function(){
-        let listOfRest = $(this).children()
-        let count = 0
-        listOfRest.each(function(i, elem) {
-            let title = new restoName({ name: $(this).children().attr('attr-gtm-title') });
-            title.save(function (err, product) {
-              if (err) console.log(err, product)
-            })
-          });
-
+    $('div.pager-wrapper div.item-list-first div.item-list .pager').filter(function(){
+      nmbrOfPages = parseInt($(this).children().last().prev().children().attr('attr-page-number'))
+      console.log(nmbrOfPages)
     })
-    let doneCounter = 0
-    for (let i = 2; i < nmbrOfPages-20; i++) {
-      let urlPage = url+"/page-"+i;
-      request(urlPage, function(error, response, html){
-        var $ = cheerio.load(html)
 
-        $('.poi-search-result').filter(function(){
-          let listOfRest = $(this).children()
-          let count = 0
-          listOfRest.each(function(i, elem) {
-            let title = new restoName({ name: $(this).children().attr('attr-gtm-title') });
-            title.save(function (err, product) {
-              if (err) console.log(err, product)
-            })
-            });
-          doneCounter++;
-      })
-      })
+    let doneCounter = 0
+    let links = [];
+    for (let i = 1; i < nmbrOfPages+1; i++) {
+      let urlPage = url+"/page-"+i;
+      links.push(urlPage);
     }
-    let inter = setInterval(function(){
-      console.log(doneCounter)
-      if(doneCounter == nmbrOfPages-21)
-      {
-        doneLoading = true
-        clearInterval(inter)
-      }
-    }, 1000)
-  }
-  })
+      let promises = links.map(link => {
+        let option = {
+          uri: link,
+          transform: function (body) {
+              return cheerio.load(body);
+          }
+        };
+        return rp(option)
+              .then(function($){
+                  $('.poi-search-result').filter(function(){
+                    let listOfRest = $(this).children()
+                    listOfRest.each(function(i, elem) {
+                      let title = new restoName({ name: $(this).children().attr('attr-gtm-title') });
+                      title.save(function (err, product) {
+                        if (err) console.log(err, product)
+                      })
+                    });
+                  })
+              })
+        })
+    Promise.all(promises).then(() => console.log("Done"));
+    })
 }
 
 function getNames(){
@@ -86,5 +79,6 @@ function getNames(){
 function areNamesLoaded(){
   return doneLoading;
 }
+getStarredRestoNames()
 module.exports = {getStarredRestoNames, getNames, areNamesLoaded}
 
